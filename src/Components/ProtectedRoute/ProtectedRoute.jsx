e8971e6fcb8f04a80e00b9d54492a954b72807b3
muhammadhamdi19 <hamdi.bakr2002@gmail.com>
2024-08-27 16:04:31 +0300

    final commit

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,8 @@
+import { Navigate } from "react-router-dom";
+
 const ProtectedRoute = ({ children }) => {
   if (localStorage.getItem("tkn") == null) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <h2 className="font-bold text-green-700 text-center text-7xl ">
-          Login First
-        </h2>
-      </div>
-    );
+    return <Navigate to="/login"/>
   }
   return <>{children}</>;
 };
